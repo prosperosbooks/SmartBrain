fd42b07c998072422946e5db5043150cb34d67bf
prosperosbooks <allen.emin@gmail.com>
2018-04-28 16:50:20 -0400

    front-end face detection API

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,21 +4,54 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import Clarifai from "clarifai";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
+
+const app = new Clarifai.App({
+    apiKey: "f65a09ca4cc145a1b4973df3c6e5c0b3"
+});
 
 const particlesOptions = {
     particles: {
         number: {
-          value: 30,
-          density: {
-            enable: true,
-            value_area: 800
-          }
+            value: 30,
+            density: {
+                enable: true,
+                value_area: 800
+            }
         }
     }
 };
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: "",
+            imageUrl: ""
+        };
+    }
+
+    onInputChange = event => {
+        this.setState({ input: event.target.value });
+    };
+
+    onButtonSubmit = () => {
+        this.setState({ imageUrl: this.state.input });
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function(response) {
+                // do something with response
+                console.log(
+                    response.outputs[0].data.regions[0].region_info.bounding_box
+                );
+            },
+            function(err) {
+                // there was an error
+            }
+        );
+    };
+
     render() {
         return (
             <div className="App">
@@ -26,10 +59,11 @@ class App extends Component {
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
-                {/*
-        <ImageLinkForm />
-        <FaceRecognition /> */}
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition imageUrl={this.state.imageUrl} />
             </div>
         );
     }
